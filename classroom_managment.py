9a37310f7da0caa62fb1cf9c265a204bd2818383
leah-barzel <149380944+leah-barzel@users.noreply.github.com>
2024-01-04 21:16:59 +0200

    my commit message

--- classroom_managment.py
+++ classroom_managment.py
@@ -51,7 +51,7 @@ def add_student(name, email=None):
     }
     classroom.append(student)
 
-add_student('dev','dev@gmail.com')
+###add_student('dev','dev@gmail.com')
 
 def print_details(lst):
     for _ in range(len(lst)):
@@ -68,7 +68,7 @@ def delete_student(name):
         student=classroom.pop(i)
     else:
         return
-delete_student('dev')
+###delete_student('dev')
 
 def set_email(name, email):
     if(re_index(classroom,name)):
@@ -76,7 +76,7 @@ def set_email(name, email):
         classroom[i]['email'] = email
        
 
-set_email('Bob','email@dfgdg.com')
+###set_email('Bob','email@dfgdg.com')
 
 def add_grade(name, profession, grade):
     if(re_index(classroom,name)):
@@ -84,7 +84,7 @@ def add_grade(name, profession, grade):
         d= (profession,int(grade))
         classroom[i]['grades'].append(d)
 
-add_grade('Bob','tora',100)
+###add_grade('Bob','tora',100)
 
 
 def avg_grade(name, profession):
@@ -97,7 +97,7 @@ def avg_grade(name, profession):
             else:
                 return None
     return None
-avg_grade('Bob','english')
+###avg_grade('Bob','english')
 
 
 def get_professions(name):
@@ -110,7 +110,7 @@ def get_professions(name):
     return sub
         
 
-get_professions("Bob")
+###get_professions("Bob")
 
 
 
