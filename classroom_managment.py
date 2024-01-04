9694f506069270aa8795b444c6c650a2b5d8307b
leah-barzel <149380944+leah-barzel@users.noreply.github.com>
2024-01-04 20:59:30 +0200

    my commit message

--- classroom_managment.py
+++ classroom_managment.py
@@ -37,7 +37,7 @@ def re_index(list,my_name):
     for i in range(len(list)):
         if(list[i]['name'] == my_name):
             return(i)
-    return 0
+    return 
             
 #print(re_index(classroom,'Charlie'))
 
@@ -55,19 +55,7 @@ add_student('dev','dev@gmail.com')
 
 def print_details(lst):
     for _ in range(len(lst)):
-        print(f'{lst[_]['name']} | {lst[_]['email']} | {lst[_]['grades']} \n')
-
-print_details(classroom)
-def add():  
-    """Add a new student to the classroom
-    with the following keys:
-    'name': the given name
-    'email': if email is given use it otherwise use <name>@example.com
-             in lowercase, you can use the `s.lower()` method
-    'grade': initialize with empty list
-    """
-    pass
-
+        print(f'{lst[_]["name"]} | {lst[_]["email"]} | {lst[_]["grades"]} \n')
 
 def delete_student(name):
     if(re_index(classroom, name)):
@@ -79,24 +67,16 @@ def delete_student(name):
         }
         student=classroom.pop(i)
     else:
-        print('the name is noe exist!')
         return
 delete_student('dev')
-print('is delete')
-print_details(classroom)
-
-
- 
 
 def set_email(name, email):
     if(re_index(classroom,name)):
         i=re_index(classroom,name)
         classroom[i]['email'] = email
-        print(classroom[i]['email'])
+       
 
 set_email('Bob','email@dfgdg.com')
-print('email')
-print_details(classroom)
 
 def add_grade(name, profession, grade):
     if(re_index(classroom,name)):
@@ -105,29 +85,29 @@ def add_grade(name, profession, grade):
         classroom[i]['grades'].append(d)
 
 add_grade('Bob','tora',100)
-print('grade')
-print_details(classroom)
-
-
-
 
 
 def avg_grade(name, profession):
-    avg=0
-    key = profession
-    i=re_index(classroom,name)
-    grade = classroom[i]['grades']
-    for _ in range(len(grade)):
-        if(grade[_]==profession):
-            print('good')
-            avg+=grade[pro]
-    avg/=_
-    print(f' the avg is {avg}')
-
+    for student in classroom:
+        if student['name'] == name:
+            grades = [grade for subj, grade in student['grades'] if subj == profession]
+            if len(grades) > 0:
+                gpa = sum(grades) / len(grades)
+                return gpa
+            else:
+                return None
+    return None
 avg_grade('Bob','english')
 
 
 def get_professions(name):
-    """Returns a list of unique professions that student has grades in"""
-    pass
+    sub = set()
+    i = re_index(classroom,name)
+    grade=classroom[i]['grades']
+    for s,_ in grade:
+        sub.add(s)
+
+get_professions("Bob")
+
+
 
